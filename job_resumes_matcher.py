8e35a2ec14dafaae1ebd85b0c0e347f6dbba2c9f
Arnav Kumar <arunav.jsr.0604@gmail.com>
2025-10-12 22:33:31 +0530

    Update job_resumes_matcher.py

--- job_resumes_matcher.py
+++ job_resumes_matcher.py
@@ -1,10 +1,13 @@
-"""Ai matches resumes  with job description and rank candidates"""
+# """Ai matches resumes  with job description and rank candidates"""
 
-import json 
-import re
-from typing import Dict, List, Tuple
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import spacy
-from collections import Counter
+# import json 
+# import re
+# from typing import Dict, List, Tuple
+# import numpy as np
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
+# import spacy
+
+# from collections import Counter
+
+
